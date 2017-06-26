9c738d4affb2d3e072d661a3f8384bcaee189580
Romeo Palacios <romeopalacios@ymail.com>
2017-06-26 12:40:15 -0700

    username function added

---
+++ app/scripts/controllers/UsernamePromptCtrl.js
@@ -0,0 +1,21 @@
+(function(){
+
+  function UsernamePromptCtrl($uibModalInstance, $cookieStore) {
+
+    this.submit = function() {
+      if(this.text) {
+
+        $cookieStore.put('blocChatCurrentUser', this.text);
+        this.text = '';
+        $uibModalInstance.close();
+      }
+
+    };
+
+
+  }
+
+  angular
+    .module('blocChat')
+    .controller('UsernamePromptCtrl',['$uibModalInstance','$cookieStore', UsernamePromptCtrl]);
+})();
---
+++ app/scripts/services/BlocChatCookies.js
@@ -0,0 +1,24 @@
+(function() {
+  function BlocChatCookies($cookies, $cookieStore, $uibModal) {
+
+    this.currentUser = $cookies.get('blocChatCurrentUser');
+
+    if (!this.currentUser || this.currentUser === '') {
+      $uibModal.open({
+        templateUrl: '/templates/usernameprompt.html',
+        controller: 'UsernamePromptCtrl as user',
+        backdrop: 'static',
+        keyboard: false
+      });
+    }
+    
+    this.setCurrentUser = function(username) {
+      $cookieStore.put('blocChatCurrentUser', username);
+      this.currentUser = username;
+    }
+  }
+
+  angular
+    .module('blocChat')
+    .run(['$cookies','$cookieStore', '$uibModal', BlocChatCookies]);
+})();
